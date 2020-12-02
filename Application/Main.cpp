e6665faa0518c286e1601c41488a27e9ae7f4b3b
Matthew Mason <matthewmason7@outlook.com>
2020-12-02 15:24:53 -0700

    added gouraud .vert and .frag

--- Application/Main.cpp
+++ Application/Main.cpp
@@ -8,98 +8,128 @@ int main(int argc, char** argv) {
 	nc::Engine engine;
 	engine.Startup();
 
-	/*nc::Renderer renderer;
-	renderer.Startup();
-	renderer.Create("OpenGL", 800, 600);*/
+	static float vertices[] =
+	{
+		-1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f,
+		 1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f,
+		 1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f,
+		 1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f,
+		-1.0f,  1.0f, -1.0f,  0.0f,  0.0f, -1.0f,
+		-1.0f, -1.0f, -1.0f,  0.0f,  0.0f, -1.0f,
 
-	//initialization
-	//float vertices[] =
-	//{
-	//	-0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, //point 1
-	//	 0.0f,  0.5f, 0.0f, 0.0f, 1.0f, 0.0f, 0.5f, 1.0f, //point 2
-	//	 0.5f, -0.5f, 0.0f, 0.0f, 0.0f, 1.0f, 1.0f, 0.0f  //point 3
+
+
+		-1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f,
+		 1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f,
+		 1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f,
+		 1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f,
+		-1.0f,  1.0f,  1.0f,  0.0f,  0.0f,  1.0f,
+		-1.0f, -1.0f,  1.0f,  0.0f,  0.0f,  1.0f,
+
+
+
+		-1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f,
+		-1.0f,  1.0f, -1.0f, -1.0f,  0.0f,  0.0f,
+		-1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f,
+		-1.0f, -1.0f, -1.0f, -1.0f,  0.0f,  0.0f,
+		-1.0f, -1.0f,  1.0f, -1.0f,  0.0f,  0.0f,
+		-1.0f,  1.0f,  1.0f, -1.0f,  0.0f,  0.0f,
+
+
+
+		 1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f,
+		 1.0f,  1.0f, -1.0f,  1.0f,  0.0f,  0.0f,
+		 1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,
+		 1.0f, -1.0f, -1.0f,  1.0f,  0.0f,  0.0f,
+		 1.0f, -1.0f,  1.0f,  1.0f,  0.0f,  0.0f,
+		 1.0f,  1.0f,  1.0f,  1.0f,  0.0f,  0.0f,
+
+
+
+		-1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f,
+		 1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f,
+		 1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f,
+		 1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f,
+		-1.0f, -1.0f,  1.0f,  0.0f, -1.0f,  0.0f,
+		-1.0f, -1.0f, -1.0f,  0.0f, -1.0f,  0.0f,
+
+
+
+		-1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f,
+		 1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f,
+		 1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+		 1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+		-1.0f,  1.0f,  1.0f,  0.0f,  1.0f,  0.0f,
+		-1.0f,  1.0f, -1.0f,  0.0f,  1.0f,  0.0f
+	};
+
+	//static float vertices[] = {
+	//	//front
+	//	-1.0f, -1.0f,  1.0f, 0.0f, 0.0f,
+	//	 1.0f, -1.0f,  1.0f, 1.0f, 0.0f,
+	//	 1.0f,  1.0f,  1.0f, 1.0f, 1.0f,
+	//	-1.0f,  1.0f,  1.0f, 0.0f, 1.0f,
+	//	//back
+	//	-1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
+	//	 1.0f, -1.0f, -1.0f, 0.0f, 0.0f,
+	//	 1.0f,  1.0f, -1.0f, 0.0f, 1.0f,
+	//	-1.0f,  1.0f, -1.0f, 1.0f, 1.0f,
 	//};
 
-	static float vertices[] = {
-		//front
-		-1.0f, -1.0f,  1.0f, 0.0f, 0.0f,
-		 1.0f, -1.0f,  1.0f, 1.0f, 0.0f,
-		 1.0f,  1.0f,  1.0f, 1.0f, 1.0f,
-		-1.0f,  1.0f,  1.0f, 0.0f, 1.0f,
-		//back
-		-1.0f, -1.0f, -1.0f, 1.0f, 0.0f,
-		 1.0f, -1.0f, -1.0f, 0.0f, 0.0f,
-		 1.0f,  1.0f, -1.0f, 0.0f, 1.0f,
-		-1.0f,  1.0f, -1.0f, 1.0f, 1.0f,
-	};
-
-	static GLushort indices[] = {
-		// front
-		0, 1, 2,
-		2, 3, 0,
-		//right
-		1, 5, 6,
-		6, 2, 1,
-		//back
-		7, 6, 5,
-		5, 4, 7,
-		//left
-		4, 0, 3,
-		3, 7, 4,
-		//bottom
-		4, 5, 1,
-		1, 0, 4,
-		//top
-		3, 2, 6,
-		6, 7, 3
-	};
+	//static GLushort indices[] = {
+	//	// front
+	//	0, 1, 2,
+	//	2, 3, 0,
+	//	//right
+	//	1, 5, 6,
+	//	6, 2, 1,
+	//	//back
+	//	7, 6, 5,
+	//	5, 4, 7,
+	//	//left
+	//	4, 0, 3,
+	//	3, 7, 4,
+	//	//bottom
+	//	4, 5, 1,
+	//	1, 0, 4,
+	//	//top
+	//	3, 2, 6,
+	//	6, 7, 3
+	//};
 
 	nc::Program program;
-	program.CreateShaderFromFile("shaders\\basic.vert", GL_VERTEX_SHADER);
-	program.CreateShaderFromFile("shaders\\basic.frag", GL_FRAGMENT_SHADER);
+	program.CreateShaderFromFile("shaders\\gouraud.vert", GL_VERTEX_SHADER);
+	program.CreateShaderFromFile("shaders\\gouraud.frag", GL_FRAGMENT_SHADER);
 	program.Link();
 	program.Use();
 
-	////create vertex buffers
-	/*GLuint vbo;
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);*/
-	nc::VertexIndexArray vertexArray;
+	/*nc::VertexIndexArray vertexIndexArray;
+	vertexIndexArray.Create("vertex");
+	vertexIndexArray.CreateBuffer(sizeof(vertices), sizeof(vertices) / (sizeof(float) * 5), vertices);
+	vertexIndexArray.SetAttribute(0, 2, 1, 0);
+	vertexIndexArray.SetAttribute(0, 4, 3, 5);*/
+
+	nc::VertexArray vertexArray;
 	vertexArray.Create("vertex");
-	vertexArray.CreateBuffer(sizeof(vertices), sizeof(vertices) / (sizeof(float) * 5), vertices);
-	vertexArray.SetAttribute(0, 2, 1, 0);
-	vertexArray.SetAttribute(0, 4, 3, 5);
-
-	//set position pipeline (vertex attribute)
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), 0);
-	glEnableVertexAttribArray(0);
-	
-	//set color pipeline (vertex attribute)
-	//glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-	//glEnableVertexAttribArray(1);
-
-	//set uv pipeline (vertex attribute)
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-	glEnableVertexAttribArray(1);
-
-	//create index buffers
-	/*GLuint ibo;
-	glGenBuffers(1, &ibo);
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);*/
-	vertexArray.CreateIndexBuffer(GL_UNSIGNED_SHORT, sizeof(indices) / sizeof(GLushort), indices);
+	vertexArray.CreateBuffer(sizeof(vertices), sizeof(vertices) / (sizeof(float) * 6), vertices);
+	vertexArray.SetAttribute(0, 3, 6 * sizeof(float), 0);
+	vertexArray.SetAttribute(1, 3, 6 * sizeof(float), 3 * sizeof(float));
 
 	//uniform
 	glm::mat4 model = glm::mat4(1.0f);
-	
 	glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800 / 600.0f, 0.01f, 1000.0f);
-	
 	glm::vec3 eye{0, 0, 5};
 	glm::mat4 view = glm::lookAt(eye, glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
 
 	nc::Texture texture;
 	texture.CreateTexture("Textures\\llama.jpg");
+
+	program.SetUniform("material.ambient", glm::vec3{ 1, 1, 1 });
+	program.SetUniform("material.diffuse", glm::vec3{ 1, 1, 1 });
+
+	program.SetUniform("light.ambient", glm::vec3{ 0.1f, 0.8f, 0.1f });
+	program.SetUniform("light.diffuse", glm::vec3{ 0, 0, 1 });
+	glm::vec4 light{ 5, 5, 5, 1 };
 
 	bool quit = false;
 	while (!quit) {
@@ -148,16 +178,17 @@ int main(int argc, char** argv) {
 		view = glm::lookAt(eye, eye + glm::vec3(0, 0, -1), glm::vec3(0, 1, 0));
 
 		glm::mat4 mvp = projection * view * model;
-		program.SetUniform("transform", mvp);
+		program.SetUniform("mvp", mvp);
+
+		glm::mat4 model_view = view * model;
+		program.SetUniform("model_view", model_view);
+
+		glm::vec4 position = view * light;
+		program.SetUniform("light.position", position);
 
 		engine.GetSystem<nc::Renderer>()->BeginFrame();
 
 		vertexArray.Draw();
-
-		//render triangle
-		//glDrawArrays(GL_TRIANGLES, 0, 3);
-		/*GLsizei numElements = sizeof(indices) / sizeof(GLushort);
-		glDrawElements(GL_TRIANGLES, numElements, GL_UNSIGNED_SHORT, 0);*/
 
 		engine.GetSystem<nc::Renderer>()->EndFrame();
 	}
